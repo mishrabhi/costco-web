afaef59200f9611f062aeabddc38c8f6317ee523
mishrabhi <asmyselfabhishek00@gmail.com>
2024-05-30 15:22:54 +0530

    added footer

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("about-us");
+  button.addEventListener("click", () => {
+    window.open(
+      /www.costco.com/about.html
+    );
+  });
+});
